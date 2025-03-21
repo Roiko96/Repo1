3d9c25c7ec8ddfb69c7cd1de28a3ec6cc2604b3b
Roiko96 <roitaba1996@gmail.com>
2025-03-21 10:02:28 +0200

    simple code of budget data AS a function to merge

---
+++ budget_data_func.py
@@ -0,0 +1,28 @@
+# Description: This file contains the functions to add a new budget data, add a new transection, get the balance and get the transections
+import budget_data
+
+
+
+# Function to add a new budget data
+def budget_data_func(budget_data):
+    return
+
+
+
+
+# Function to add a new transection
+def add_transection(transection):
+    return
+
+
+
+# Function to get the balance
+def get_balance(balance):
+    return
+
+
+
+
+# Function to get the history transections
+def get_transections(transections.history):
+    return 
